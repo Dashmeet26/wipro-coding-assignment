e276b3c03cfa8164fbabd717aa49503417964c1a
Dashmeet Singh <dashmeet.singh@franklintempleton.com>
2020-10-14 15:35:48 +0530

    -- initial commit

---
+++ CodingAssignment/CodingAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodingAssignment
+//
+//  Created by FT User on 14/10/20.
+//  Copyright © 2020 FT User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
